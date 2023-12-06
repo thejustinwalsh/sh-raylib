e3aa1e0f6ecddcae3958d0cfa6dbbfca87988048
Justin Walsh <contact.me@thejustinwalsh.com>
2023-12-05 21:48:53 -0500

    chore: type stubs

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,6 @@
-interface c_ptr { __kind: "c_ptr"; }
+declare type c_ptr = { __kind: "c_ptr"; };
+declare type c_int = number;
+declare type c_float = number;
+declare type c_uchar = number;
 
 declare function print(log: string): void;
